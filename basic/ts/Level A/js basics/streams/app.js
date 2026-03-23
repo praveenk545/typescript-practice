c6d5dee0da9aef3d35b3cecf4fb6a8425ba5016f
praveenk545 <kumarp71149@gmail.com>
2026-03-23 21:26:04 +0530

    node js - streams : readable and writable streams

--- basic/ts/Level A/js basics/streams/app.js
+++ basic/ts/Level A/js basics/streams/app.js
@@ -2,12 +2,28 @@ const fs=require("fs/promises");
 
 
 (async()=>{
- const handler = fs.open('./writ.text',"w");
+ const handler = await fs.open('./writ.text',"w");
  const start = performance.now();
- for (let i=0;i<=10;i++){
+ for (let i=0;i<=1000000;i++){
     // const buff =Buffer.from(`${i}`,'utf');
     (await handler).write(`${i}`);
  }
  const end =performance.now();
  console.log(end-start, ": MS")
 })();
+
+
+// (async()=>{
+//  const handler = await fs.open('./writ.text',"w");
+//  const stream = handler.createWriteStream();
+
+//  const start = performance.now();
+//  for(let i=0;i<=100000;i++){
+//    const buff = Buffer.from(`${i}`, "utf-8");
+//    stream.write(buff);
+//  }
+//  const end = performance.now();
+//  console.log(end-start, ": MS")
+// })();
+
+// stream : an abstract interface for working with streaming data in Node.js
